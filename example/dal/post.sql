b75922a246eeace69467a10d2d12e0cf27327727
Nebojsa Vislavski <will@secondsurfer.com>
2026-02-23 18:24:11 -0500

    added generate context

--- example/dal/post.sql
+++ example/dal/post.sql
@@ -2,7 +2,7 @@ CREATE TABLE posts (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     version INT DEFAULT 0,
     deleted BOOLEAN NOT NULL,
-    post VARCHAR(255) NOT NULL,
+    post text NOT NULL,
     target_age TINYINT NOT NULL,
     created TIMESTAMP,
     updated TIMESTAMP
@@ -11,7 +11,7 @@ CREATE TABLE posts (
 # Unique indexes as they serve Get operations returning single entity
 
 # Indexes that serve all the list operations
-CREATE INDEX idx_deleted ON posts (deleted);
 CREATE INDEX idx_target_age ON posts (target_age);
+CREATE INDEX idx_deleted ON posts (deleted);
 CREATE INDEX idx_created ON posts (created);
 
